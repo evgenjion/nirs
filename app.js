76ce0c5fe562d573a9ebf1e95ee860e871c7db6d
Евгений Мищенко <evgenjion@yandex-team.ru>
2015-02-15 20:23:38 +0300

    add project structure

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    jade = require('jade'),
+    app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+     res.send(jade.renderFile('views/index.jade', {
+        // can be used in index.jade
+        param: 'param qwerty ololo'
+     }));
+});
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
