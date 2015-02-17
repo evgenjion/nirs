7e72893f204cbb859060fae123462602e3cf7403
Евгений Мищенко <evgenjion@yandex-team.ru>
2015-02-17 21:30:09 +0300

    add simple gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    stylus = require('gulp-stylus');
+
+// Собираем Stylus
+gulp.task('stylus', function() {
+    gulp.src('./src/styl/*.styl')
+        // склеиваем, чтобы подтянулись конфиги
+        .pipe(concat('style.styl'))
+        .pipe(stylus(
+         // { use: ['nib'] }
+        )) // собираем stylus
+    .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+    //.pipe(myth()) // добавляем префиксы - http://www.myth.io/
+    .pipe(gulp.dest('./public/css/')); // записываем css
+});
+
+gulp.task('default', ['stylus'], function() {
+    // place code for your default task here
+});
