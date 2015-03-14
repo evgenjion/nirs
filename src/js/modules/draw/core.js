f3b55accf1302609f291a2a70b5629259c688f5c
Евгений Мищенко <evgenjion@yandex-team.ru>
2015-03-14 18:25:45 +0300

    simplify shapes constructor

--- src/js/modules/draw/core.js
+++ src/js/modules/draw/core.js
@@ -1,18 +1,83 @@
 define('draw/core', [], function() {
 
+    // TODO: extend
+    var Rect = function(left, top) {
+        var width = 1,
+            height = 1;
+
+        return {
+            update: function(x, y) {
+                width = x;
+                height = y;
+
+                return this.getParams();
+            },
+            getParams: function() {
+                return {
+                    width: width,
+                    height: height,
+                    left: left,
+                    top: top
+                };
+            }
+        };
+    };
+
+    var Ellipse = function(left, top) {
+        var rx = 1,
+            ry = 1;
+
+        return {
+            update: function(x, y) {
+                rx = x;
+                ry = y;
+
+                return this.getParams();
+            },
+            getParams: function() {
+                return {
+                    rx: rx,
+                    ry: ry,
+                    left: left + rx/2,
+                    top: top + ry/2
+                };
+            }
+        };
+    };
+
+    // TODO: correct params
+    var Line = function(left, top) {
+        var x1 = left,
+            y1 = top,
+            x2 = left+1,
+            y2 = top+1;
+
+        return {
+            update: function(x, y) {
+                x2 = x;
+                y2 = y;
+            },
+            getParams: function() {
+                return {
+                    x1: x1,
+                    x2: x2,
+                    y1: y1,
+                    y2: y2
+                };
+            }
+        };
+    }
+
     /**
      * returns params for drawing object(width, height for Rect, rx, ry for Ellipse)
      */
     function getObjParams(type) {
-        if (~['Rect', 'Triangle'].indexOf(type)) return {
-            x: 'width',
-            y: 'height'
-        };
+        // TODO: прокидывать стартовую позицию
+        if (~['Rect', 'Triangle'].indexOf(type)) return Rect;
 
-        if (type === 'Ellipse') return {
-            x: 'rx',
-            y: 'ry'
-        };
+        if (type === 'Ellipse') return Ellipse;
+
+        if (type === 'Line') return Line;
     };
 
     return {
@@ -25,14 +90,8 @@ define('draw/core', [], function() {
          */
         createDrawingObj: function(canvas, params) {
             console.log(params.type);
-            var drawingObjParams = getObjParams(params.type),
-                drawingObj;
-
-            // 1 because of strange rendering by fabric.js with width === 0
-            params[drawingObjParams.x] = 1;
-            params[drawingObjParams.y] = 1;
-
-            drawingObj = new fabric[params.type](params);
+            var drawingObjParams = getObjParams(params.type)(params.left, params.top),
+                drawingObj = new fabric[params.type](drawingObjParams.getParams());
 
             canvas.add(drawingObj);
 
@@ -43,10 +102,7 @@ define('draw/core', [], function() {
                  * @param {Number} params.top y coord of mouseup
                  */
                 update: function(params) {
-                    var setParams = {};
-
-                    setParams[drawingObjParams.x] = params.left;
-                    setParams[drawingObjParams.y] = params.top;
+                    var setParams = drawingObjParams.update(params.left, params.top);
 
                     drawingObj.set(setParams);
                 }
