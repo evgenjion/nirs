0cd3fa3206098700958e94e438b692f659b988ee
Евгений Мищенко <evgenjion@yandex-team.ru>
2015-11-03 21:43:36 +0300

    add customer ws module
    todo: separate module logic to transport and drawing

--- frontend/js/modules/common/ws.ts
+++ frontend/js/modules/common/ws.ts
@@ -11,5 +11,6 @@ export class WebsocketTransport {
         DRAW_END: 'DRAW_END'
     };
 
+    // FIXME: нормальный адрес
     protected socket = new WebSocket("ws://localhost:8081/draw");
 }
--- frontend/js/modules/customer/ws.ts
+++ frontend/js/modules/customer/ws.ts
@@ -5,14 +5,36 @@
 
 import Base = require('common/ws');
 import Utils = require('core/utils');
+import DrawController = require('draw/controller');
 
-class WebSocketClient extends Base.WebsocketTransport{
+declare var Utils:any;
+
+class WebSocketClient extends Base.WebsocketTransport {
+    private observer:Observer;
+
     constructor() {
         super();
-        this.socket.onopen = function() {
+
+
+        // TODO: порефакторить
+        let canvas = DrawController.getCanvas();
+
+        canvas.selection = false;
+
+        setInterval(function() {
+            canvas.renderAll();
+        }, 15);
+
+
+
+
+        this.socket.onopen = () => {
             // TODO: WebsocketTransport реюзать код (Лучше вообще сунуть это  utils)
             let location = window.location.href.toString();
             let data = location.slice(location.lastIndexOf('/') + 1);
+
+
+            console.log('socket init!');
 
             this.socket.send(JSON.stringify({
                 type: this.types.CONNECT,
@@ -20,9 +42,41 @@ class WebSocketClient extends Base.WebsocketTransport{
             }));
         };
 
-        this.socket.onmessage = function () {
-            console.log(arguments);
+        this.socket.onmessage = (e) => {
+            let msg = JSON.parse(e.data);
+            let data = msg.data;
+
+            console.log(data);
+
+            //TODO: выпилить отсюда эту логику, оставить только транспортную
+            switch (msg.type) {
+                case this.types.DRAW_START:
+                    DrawController.setDrawType(data.shape);
+                    DrawController.drawingStart(formatCoords(data.coords));
+                    break;
+                case this.types.DRAW_PROGRESS:
+                    console.log(formatCoords(data.coords));
+                    DrawController.drawingUpdate(formatCoords(data.coords));
+                    break;
+                case this.types.DRAW_END:
+                    DrawController.drawingEnd();
+                    break;
+                default:
+                    console.error('unsupported data: ', data);
+
+            }
+
+            function formatCoords(coords: Array<number>) {
+                let [left, top] = coords;
+                return { left, top };
+            }
         };
+
+        this.observer = new Utils.EventSupport();
+    }
+
+    public onDrawUpdate(callb:Function):void {
+        this.observer.on('draw-update', callb);
     }
 }
 
