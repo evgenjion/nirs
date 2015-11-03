f7ece0d23fe062a1737d1b708a9d7c5d3f8de653
Евгений Мищенко <evgenjion@yandex-team.ru>
2015-11-03 21:35:35 +0300

    send data to connected customers

--- backend/socket-server.js
+++ backend/socket-server.js
@@ -12,6 +12,8 @@ module.exports = (sessionHandler) => {
         port: 8081
     });
 
+    let connections = {};
+
     webSocketServer.on('connection', function(ws) {
         let req = ws.upgradeReq;
         let res = { writeHead: {} }; // Particularly nasty
@@ -19,8 +21,27 @@ module.exports = (sessionHandler) => {
         sessionHandler(req, res, function (err) {
             ws.on('message', function(message) {
                 console.log('получено сообщение ' + message);
+
+                let info = JSON.parse(message);
+
+                switch (info.type) {
+                    // TODO: typedef хотябы
+                    case 'CONNECT':
+                        let boardID = info.data
+                        req.session.currentBoard = boardID;
+                        connections[boardID] || (connections[boardID] = []);
+                        connections[boardID].push(ws);
+                        break;
+
+                    default:
+                        connections[req.session.currentBoard].forEach(function(ws, i) {
+                            ws.send(JSON.stringify(info));
+                        });
+                        break;
+                }
             });
 
+            // TODO: выпилить из connections
             ws.on('close', function() {
                 console.log('соединение закрыто ' + req.sessionID);
             });
