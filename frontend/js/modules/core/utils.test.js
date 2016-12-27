a3caa553b13024259a856575c5b4e97bc3ccb85f
Евгений Мищенко <evgenjion@yandex-team.ru>
2016-12-27 22:22:30 +0300

    update tests for core/utils
    add testcases for Observer

--- frontend/js/modules/core/utils.test.js
+++ frontend/js/modules/core/utils.test.js
@@ -4,8 +4,6 @@ let _ = require('lodash'),
     sinon = require('sinon'),
     {assert} = require('chai');
 
-
-
 describe('Utils module', function() {
     let Utils;
     
@@ -22,7 +20,7 @@ describe('Utils module', function() {
     });
 
     after(function(){
-        
+        global.window = undefined;
     });
 
     describe('#getCurrentBoardId()', function() {
@@ -32,12 +30,65 @@ describe('Utils module', function() {
     });
 
     describe('#EventSupport()', function() {
-        let Observer;
+        let Observer,
+            handler,
+            handler1;
 
-        before(function() {
+        beforeEach(function() {
             Observer = new Utils.EventSupport();
+            handler = sinon.stub();
+            handler1 = sinon.stub();
         });
 
-        // TODO:
+        afterEach(function() {
+            sinon.restore();
+        });
+
+        it('should add handler to an event and call on trigger', function(){
+            const eventName = 'EVENT_NAME';
+
+            Observer.on(eventName, handler);
+            Observer.trigger(eventName);
+
+            assert(handler.calledOnce, 'Handler wasnt called after combination of .on(e, handler) and trigger(e)');
+        });
+
+        it('should add handler and remove it with .un()', function(){
+            const eventName = 'EVENT_NAME';
+
+            Observer.on(eventName, handler);
+            Observer.un(eventName, handler);
+
+            Observer.trigger(eventName);
+
+            assert(handler.notCalled, 'Handler was called after combination of .on(e, handler) and un(e, handler)');
+        });
+
+        it('should remove only one handler with .un()', function(){
+            const eventName = 'EVENT_NAME';
+
+            Observer.on(eventName, handler);
+            Observer.on(eventName, handler1);
+            Observer.un(eventName, handler);
+
+            Observer.trigger(eventName);
+
+            assert(handler.notCalled, 'Handler was called after combination of .on(e, handler) and un(e, handler)');
+            assert(handler1.calledOnce, 'Handler wasnt called after combination of .on(e, handler) .un(e, AnotherHandler) and trigger(e)');
+        });
+
+        it('should remove all handlers with .off()', function(){
+            const eventName = 'EVENT_NAME';
+
+            Observer.on(eventName, handler);
+            Observer.on(eventName, handler1);
+
+            Observer.off(eventName);
+
+            Observer.trigger(eventName);
+
+            assert(handler.notCalled, 'Handler was called after combination of .on(e, handler) and off(e)');
+            assert(handler1.notCalled, 'Handler was called after combination of .on(e, handler) and off(e)');
+        });
     })
 });
