ec65de6b3226b6240f2e6aa783b7efcca38a64f2
Евгений Мищенко <evgenjion@yandex-team.ru>
2016-12-27 22:20:01 +0300

    requirejs add libs path to config

--- frontend/js/modules/customer-main.ts
+++ frontend/js/modules/customer-main.ts
@@ -1,2 +1,10 @@
+requirejs.config({
+    paths: {
+        lodash: '/public/js/libs/lodash/dist/lodash.min.js',
+        fabric: '/public/js/libs/fabric.js/dist/fabric.js',
+        zepto: '/public/js/libs/zepto.min.js'
+    }
+});
+
 require(['customer/ws'], ()=> {
 });
--- frontend/js/modules/main.js
+++ frontend/js/modules/main.js
@@ -1,1 +1,9 @@
+requirejs.config({
+    paths: {
+        lodash: '/public/js/libs/lodash/dist/lodash.min.js',
+        fabric: '/public/js/libs/fabric.js/dist/fabric.js',
+        zepto: '/public/js/libs/zepto.min.js'
+    }
+});
+
 require(['draw/click-bind'], function(core) {});
