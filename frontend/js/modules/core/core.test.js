045fa4b6b61c128858dd5665fa212880274ef3a4
Евгений Мищенко <evgenjion@yandex-team.ru>
2016-12-29 19:11:04 +0300

    Add unit test for core/core

---
+++ frontend/js/modules/core/core.test.js
@@ -0,0 +1,15 @@
+describe('Core module', function(){
+    let Module, Utils;
+    before(function(done){
+        // Импорт amd модуля
+        requirejs(['core/core', 'core/utils'], function (core, utils) {
+            Module = core;
+            Utils = utils;
+            done();
+        });
+    });
+
+    it('should return instance of Utils.EventSupport', function(){
+        assert.instanceOf(Module, Utils.EventSupport);
+    });
+});
